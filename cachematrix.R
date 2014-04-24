4f4cc66e932d5ea43c31fd0b26aa3c5a4b6772e3
Gene Cho <genec2008@gmail.com>
2014-04-24 07:02:24 -0700

    Submitted version
    works!

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming - April 2014
+## Programming Assignment 2 - Caching the Inverse of a Matrix
+## 
 
-## Function to create an object for 
-## caching a matrix and its inverse
+# In this example we introduce the <<- operator which can be used to assign a
+# value to an object in an environment that is different from the current
+# environment. 
+
+# Below are two functions that are used to create a special object
+# that stores a matrix and cache's its inverse.
+# 
+# The first function, makeVector creates a special "vector", which is really a
+# list containing a function to:
+# - set the matrix
+# - get matrix
+# - set matrix inverse
+# - get matrix inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
         
@@ -21,17 +34,17 @@ makeCacheMatrix <- function(x = matrix()) {
         list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-# Function to calculate the inverse of the object.
-# checks to see if the inverse has already been calculated. 
-# if not, it calculates the inverse and stores it in the object 
+
+# The second function calculates the inverse of the object. It checks to see if the inverse
+# has already been calculated. Ff not, it calculates the inverse and stores it
+# in the object.  Returns a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         
         inverse <- x$getInverse()               # retrieve cached inverse
         if(!is.na(inverse[1,1])) {              # if not empty, return cached inverse       
-                message("getting cached 
-                        inverse matrix")
+                message("getting cached inverse matrix")
+                
                 return(inverse)                 # use invisible(inverse) to suppress print()
         }
         data <- x$get()                         # otherwise get cached data
@@ -41,19 +54,20 @@ cacheSolve <- function(x, ...) {
         
 }
 
-# use functions to generate inverse matrix
+# Now use these functions to generate an inverse matrix and cache it. Verify that
+# the calculated and cached inverses are identical Verify that the matrix
+# multiplied by its inverse is equal to the identity matrix
 
-size <- 10                                   # create data (square matrix)
+size <- 10                                       # create test data (square matrix)
 mydata <- matrix(rnorm(size*size), size, size)
 
-M1 <- makeCacheMatrix()                         # create caching data structure 
-M1
-environment(M1)
+mat <- makeCacheMatrix()                         # create caching data structure 
+mat
 
-M1$set(mydata)                                  # place data into structure
-M1$getInverse()                                 # get inverse (empty at this point)
-imat <- cacheSolve(M1)                          # calculate the inverse
-imat_cached <- cacheSolve(M1)                   # caculate the inverse again (retrieved cached value)
+mat$set(mydata)                                  # place data into structure
+mat$getInverse()                                 # get inverse (empty at this point)
+imat <- cacheSolve(mat)                          # calculate the inverse
+imat_cached <- cacheSolve(mat)                   # caculate the inverse again (retrieved cached value)
 
-identical(imat, imat_cached)                    # verify inverse and cached inversed are the same
-test <- mydata %*% imat                         # verify matrix %*% inverse = identity matrix
+identical(imat, imat_cached)                     # verify inverse and cached inversed are the same
+test <- mydata %*% imat                          # verify matrix %*% inverse = identity matrix
